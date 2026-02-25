df15a3458db91938e0ddd957802149cc27138db0
Ngoc Ngo <ngocnv371@gmail.com>
2026-02-25 20:51:23 +0700

    correct page url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/storyflow',  // Use relative paths for GitHub Pages
+      base: '/StoryFlow',  // Use relative paths for GitHub Pages
       server: {
         port: 3000,
         host: '0.0.0.0',
