15e1c7ddc0db87a1971a9d305d030cbec090dcb9
Ngoc Ngo <ngocnv371@gmail.com>
2026-04-02 17:46:59 +0700

    correct key usages

--- global.d.ts
+++ global.d.ts
@@ -10,6 +10,11 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_AUDIO_BUCKET?: string;
   readonly VITE_SUPABASE_MUSIC_BUCKET?: string;
   readonly VITE_SUPABASE_VIDEO_BUCKET?: string;
+  readonly GEMINI_API_KEY?: string;
+  readonly OPENROUTER_API_KEY?: string;
+  readonly YOUTUBE_OAUTH_CLIENT_ID?: string;
+  readonly VITE_SUPABASE_URL?: string;
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY?: string;
 }
 
 interface ImportMeta {
